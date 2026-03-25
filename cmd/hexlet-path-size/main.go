f3de1b56f64a078699690f449124a983518b6afd
adammilligan <millgan83949@gmail.com>
2026-03-25 13:00:45 +0300

    третий шаг

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli/v3"
+
+	"code"
 )
 
 func main() {
 	cmd := &cli.Command{
 		Name:  "hexlet-path-size",
 		Usage: "print size of a file or directory",
+		Action: func(_ context.Context, command *cli.Command) error {
+			path := command.Args().First()
+			if path == "" {
+				return cli.Exit("path is required", 1)
+			}
+
+			size, err := code.GetSize(path)
+			if err != nil {
+				return cli.Exit(err.Error(), 1)
+			}
+
+			fmt.Printf("%dB\t%s\n", size, path)
+			return nil
+		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
--- path_size.go
+++ path_size.go
@@ -1,1 +1,35 @@
-package main
+package code
+
+import "os"
+
+func GetSize(path string) (int64, error) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return 0, err
+	}
+
+	if !info.IsDir() {
+		return info.Size(), nil
+	}
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return 0, err
+	}
+
+	var total int64
+	for _, entry := range entries {
+		entryInfo, err := entry.Info()
+		if err != nil {
+			return 0, err
+		}
+
+		if entryInfo.IsDir() {
+			continue
+		}
+
+		total += entryInfo.Size()
+	}
+
+	return total, nil
+}
